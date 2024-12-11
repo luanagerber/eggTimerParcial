2624582297550cb2dc584b2ccf71a88a43f85111
Luana Gerber <luanagerber.dev@gmail.com>
2024-12-11 14:15:17 -0400

    HomeView edited to add activity

--- EggTimer.swiftpm/Views/HomeView.swift
+++ EggTimer.swiftpm/Views/HomeView.swift
@@ -13,73 +13,76 @@ import SwiftUI
 
 struct HomeView: View {
     var body: some View {
-        NavigationStack { // Gerencia a navegação entre telas.
-            ZStack {
-                Constants.backgroundColor
-                    .ignoresSafeArea() // Define a cor de fundo ocupando toda a tela e ignorando a Safe Area.
-                
-                Image("Background") // Imagem de fundo decorativa.
-                    .resizable()
-                    .opacity(0.15) // Define a transparência da imagem.
-                    .aspectRatio(contentMode: .fill)
-                    .ignoresSafeArea()
-                
-                VStack(spacing: 20) {
-                    Spacer()
-                    
-                    // Título principal da tela.
-                    Text("Welcome to \nEgg Timer!")
-                    // o \n adiciona a quebra de linha.
-                        .font(Constants.titleFont)
+        NavigationStack {
+            
+            // EDITAR A PARTIR DESSA LINHA
+            
+            
+            Constants.backgroundColor
+                .ignoresSafeArea() // Define a cor de fundo ocupando toda a tela e ignorando a Safe Area.
+            
+            
+            // Imagem de fundo decorativa.
+            Image("Background")
+                .resizable()
+                .opacity(0.15)
+                .aspectRatio(contentMode: .fill)
+                .ignoresSafeArea()
+            
+            
+            // Título principal da tela.
+            Text("Welcome to \nEgg Timer!")
+                .font(Constants.titleFont)
+                .fontWeight(.semibold)
+                .textCase(.uppercase)
+                .foregroundStyle(Constants.fontColor)
+                .multilineTextAlignment(.center)
+            
+            
+            // Imagens dos ovos para decorar a interface.
+            Image("egg_soft")
+                .resizable()
+                .frame(width: 110, height: 150)
+            
+            Image("egg_medium")
+                .resizable()
+                .frame(width: 110, height: 150)
+            
+            Image("egg_hard")
+                .resizable()
+                .frame(width: 110, height: 150)
+            
+            
+            // Subtítulo explicativo.
+            Text("We help you cook your eggs \njust the way you like them!")
+                .font(.system(size: Constants.subtitleFontSize))
+                .fontWeight(.regular)
+                .foregroundStyle(Constants.fontColor)
+                .multilineTextAlignment(.center)
+            
+            
+            // Botão que navega para a tela do temporizador (TimerView).
+            NavigationLink(
+                destination: TimerView(),
+                label: {
+                    Text("Start timer")
+                        .font(.system(size: Constants.buttonFontSize))
                         .fontWeight(.semibold)
-                        .textCase(.uppercase)
-                        .foregroundStyle(Constants.fontColor)
-                        .multilineTextAlignment(.center)
-                    
-                    // Imagens dos ovos para decorar a interface.
-                    HStack(spacing: -15) {
-                        Image("egg_soft")
-                            .resizable()
-                            .frame(width: 110, height: 150)
-                        Image("egg_medium")
-                            .resizable()
-                            .frame(width: 110, height: 150)
-                        Image("egg_hard")
-                            .resizable()
-                            .frame(width: 110, height: 150)
-                    }.padding(10)
-                    
-                    // Subtítulo explicativo.
-                    Text("We help you cook your eggs \njust the way you like them!")
-                        .font(.system(size: Constants.subtitleFontSize))
-                        .fontWeight(.regular)
-                        .foregroundStyle(Constants.fontColor)
-                        .multilineTextAlignment(.center)
-                    
-                    Spacer()
-                    
-                    // Botão que navega para a tela do temporizador (TimerView).
-                    NavigationLink(
-                        destination: TimerView(),
-                        label: {
-                            Text("Start timer")
-                                .font(.system(size: Constants.buttonFontSize))
-                                .fontWeight(.semibold)
-                                .foregroundColor(Constants.fontColor)
-                                .padding()
-                                .background(Constants.buttonBackgroundColor)
-                                .cornerRadius(Constants.buttonCornerRadius)
-                        }
-                    )
+                        .foregroundColor(Constants.fontColor)
+                        .padding()
+                        .background(Constants.buttonBackgroundColor)
+                        .cornerRadius(Constants.buttonCornerRadius)
                 }
-                .padding(.top, 70)
-                .padding(.bottom, 50)
-            }
+            )
+            
+            
+            // EDITAR SOMENTE ATÉ ESTA LINHA
+            
         }
-        .tint(Constants.fontColor) // Define a cor do botão "Voltar".
+        .tint(Constants.fontColor)
     }
 }
 
 #Preview {
-    HomeView() // Visualização da prévia da interface no Xcode.
+    HomeView()
 }
